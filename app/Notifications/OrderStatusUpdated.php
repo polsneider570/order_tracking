daac50ca8f0f5e5c6157641f37a54e156cfead3b
dev_platon <p.pobozhij@kitgroup.ua>
2024-11-21 16:30:24 +0200

    add horizon for tracking queues

--- app/Notifications/OrderStatusUpdated.php
+++ app/Notifications/OrderStatusUpdated.php
@@ -4,10 +4,11 @@ namespace App\Notifications;
 
 use App\Models\Order;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class OrderStatusUpdated extends Notification
+class OrderStatusUpdated extends Notification implements ShouldQueue
 {
     use Queueable;
 
